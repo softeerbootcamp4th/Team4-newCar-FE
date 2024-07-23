cf9a16d6ac5e16d3dfb71a17e4424164b143be83
nimod7890 <nimod1234@g.skku.edu>
2024-07-23 20:59:52 +0900

    fix: eslint + prettier setting

--- packages/admin/src/routes/router.tsx
+++ packages/admin/src/routes/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom';
 import { Button } from 'src/components/ui/button';
 import RoutePaths from 'src/constants/routePath';
 
-const routes:RouteObject[] = [{
-  path: RoutePaths.HOME,
-  element: <Button>hi</Button>,
-}];
+const routes: RouteObject[] = [
+	{
+		path: RoutePaths.HOME,
+		element: <Button>hi</Button>,
+	},
+];
 
 const router = createBrowserRouter(routes);
 
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
-  tabWidth: 2,
-  useTabs: true,
-  singleQuote: true,
-  semi: true,
-  printWidth: 120,
-  plugins: [require.resolve('prettier-plugin-tailwindcss')],
-  tailwindConfig: './tailwind.config.cjs',
-  tailwindAttributes: ['className'],
+	tabWidth: 2,
+	useTabs: true,
+	singleQuote: true,
+	semi: true,
+	printWidth: 100,
+	plugins: [require.resolve('prettier-plugin-tailwindcss')],
+	tailwindConfig: './tailwind.config.cjs',
+	tailwindAttributes: ['className'],
 };

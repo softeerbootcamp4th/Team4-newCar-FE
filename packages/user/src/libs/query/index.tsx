0855364dd85ab47f5dae95f2c0946953fe51c48b
nimod7890 <nimod1234@g.skku.edu>
2024-07-23 20:59:52 +0900

    fix: eslint + prettier setting

--- packages/user/src/libs/query/index.tsx
+++ packages/user/src/libs/query/index.tsx
@@ -4,20 +4,16 @@ import { PropsWithChildren } from 'react';
 const SECOND = 1000;
 
 export const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 1,
-      retryDelay: SECOND,
-      refetchOnWindowFocus: false,
-      staleTime: 5 * 60 * SECOND,
-    },
-  },
+	defaultOptions: {
+		queries: {
+			retry: 1,
+			retryDelay: SECOND,
+			refetchOnWindowFocus: false,
+			staleTime: 5 * 60 * SECOND,
+		},
+	},
 });
 
 export default function QueryProvider({ children }: PropsWithChildren) {
-  return (
-	<Provider client={queryClient}>
-		{children}
-	</Provider>
-  );
+	return <Provider client={queryClient}>{children}</Provider>;
 }

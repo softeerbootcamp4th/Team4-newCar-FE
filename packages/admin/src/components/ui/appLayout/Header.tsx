c2a80b0fd6bfbbdfb3b850d4432af23eae239d9b
moana16 <moana16@naver.com>
2024-07-24 14:32:42 +0900

    fix : eslint error 수정

--- packages/admin/src/components/ui/appLayout/Header.tsx
+++ packages/admin/src/components/ui/appLayout/Header.tsx
@@ -1,18 +1,20 @@
 import NavTitle from 'src/types/appLayout/types';
 
 interface HeaderProps {
-    headerTitle: string;
-    navTitles?: NavTitle[];
+	headerTitle: string;
+	navTitles?: NavTitle[];
 }
 
-function Header({ headerTitle, navTitles } : HeaderProps) {
+function Header({ headerTitle, navTitles }: HeaderProps) {
 	if (!navTitles) return <h2>{headerTitle}</h2>;
 	return (
 		<div>
 			<h2>{headerTitle}</h2>
 
 			<ul className="flex gap-2">
-				{navTitles.map(navTitle => (<li key={navTitle.navTitleid}>{navTitle.navTitle}</li>))}
+				{navTitles.map(navTitle => (
+					<li key={navTitle.navTitleid}>{navTitle.navTitle}</li>
+				))}
 			</ul>
 		</div>
 	);

5b0e3a6b4440f71b3f899fec1d72dd1822fcd8df
moana16 <moana16@naver.com>
2024-07-24 11:14:49 +0900

    style : sidebar 스타일 수정

--- packages/admin/src/components/appLayout/SideBar.tsx
+++ packages/admin/src/components/appLayout/SideBar.tsx
@@ -9,8 +9,8 @@ interface SideBarProps {
 
 function SideBar({ isChecked } : SideBarProps) {
   return (
-	<div className={`absolute w-[300px] m-[-100px_0_0_-100px] p-[50px] pt-[125px] list-none font-sans antialiased origin-[0%_0%] transform transition-transform duration-500 ease-[cubic-bezier(0.77,0.2,0.05,1.0)] ${
-        isChecked ? 'translate-x-0 ' : '-translate-x-full '
+	<div className={`absolute w-[300px] m-[-100px_0_0_-50px] h-screen px-10 pt-[125px] list-none font-sans antialiased origin-[0%_0%] bg-slate-200 transform transition-all duration-500 ease-[cubic-bezier(0.77,0.2,0.05,1.0)] ${
+        isChecked ? 'translate-x-0 opacity-100 ' : '-translate-x-full opacity-0'
       }`}
 	>
 		{SideBarRoutes.map((route) => (
@@ -28,7 +28,7 @@ function SideBar({ isChecked } : SideBarProps) {
 		</AccordionItem>
 	</Accordion>
         ) : (
-	<div key={route.id} className="border-b py-4 hover:cursor-pointer hover:underline">
+	<div key={route.id} className="border-b border-gray-400 py-4 hover:cursor-pointer hover:underline">
 		{route.name}
 	</div>
         )
--- packages/admin/src/components/appLayout/SideBarContainer.tsx
+++ packages/admin/src/components/appLayout/SideBarContainer.tsx
@@ -10,18 +10,18 @@ function SideBarContainer() {
 
   const spanStyled = 'block w-8 h-1 mb-1 relative bg-[#cdcdcd] rounded z-10 origin-[4px_0px] transition-transform transition-bg duration-500 ease-custom-bezier ';
   return (
-	<nav role="navigation" className="mt-5">
+	<nav role="navigation" className="mt-5 ml-5">
 		<div id="menuToggle" className="relative z-10 select-none">
 			<input
   type="checkbox"
   checked={isChecked}
   onChange={handleToggle}
-  className="w-10 h-8 absolute top-[-7px] left-[-5px] cursor-pointer opacity-0 z-20"
+  className="w-10 h-8 absolute top-[-7px] right-1 cursor-pointer opacity-0 z-20"
 			/>
 
 			<span
   className={`${spanStyled} ${
-        isChecked ? 'transform rotate-45 translate-x-[-2px] translate-y-[-1px] bg-[#232323]' : ''
+        isChecked ? 'transform rotate-45 translate-x-[-2px] translate-y-[-1px] bg-slate-500' : ''
       }`}
 			/>
 			<span
@@ -31,7 +31,7 @@ function SideBarContainer() {
 			/>
 			<span
   className={`${spanStyled}  ${
-        isChecked ? 'transform rotate-[-45deg] translate-x-[-2px translate-y-[-1px] bg-[#232323]' : ''
+        isChecked ? 'transform rotate-[-45deg] translate-x-[-2px translate-y-[-1px] bg-slate-500' : ''
       }`}
 			/>
 

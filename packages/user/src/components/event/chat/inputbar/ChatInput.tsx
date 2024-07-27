028c0c98e5e7ff26a670ee971ed8d77b9290d49c
nimod7890 <nimod1234@g.skku.edu>
2024-07-27 22:26:52 +0900

    feat: autocomplete off

--- packages/user/src/components/event/chat/inputbar/ChatInput.tsx
+++ packages/user/src/components/event/chat/inputbar/ChatInput.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import inputStyles from 'src/styles/input';
 import cn from 'src/utils/cn';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const ChatInput = React.forwardRef<HTMLInputElement, InputProps>(
-	({ className, type, ...props }, ref) => (
+	({ className, ...props }, ref) => (
 		<input
 			type="text"
+			autoComplete="off"
 			placeholder="기대평을 입력하세요!"
 			className={cn(
-				'placeholder:text-neutral-300 focus-visible:ring-primary !autofill:bg-neutral-800 !autofill:text-body-2!important flex h-[52px] w-[616px] rounded-2.5 bg-neutral-800 p-3 text-body-2 font-normal text-neutral-100 -shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+				inputStyles,
+				'placeholder:text-neutral-300 focus-visible:ring-primary h-[52px] w-[616px] rounded-2.5 bg-neutral-800 p-3',
 				className,
 			)}
 			ref={ref}
---
+++ packages/user/src/styles/input.ts
@@ -0,0 +1,3 @@
+const inputStyles = 'focus-visible:ring-primary text-body-2 font-normal -shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 !autofill:bg-neutral-800 !autofill:text-body-2 text-neutral-100';
+
+export default inputStyles;

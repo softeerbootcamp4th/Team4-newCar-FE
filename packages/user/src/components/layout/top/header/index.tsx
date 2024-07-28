24b621b3b22619ef678017eea089c01e99a4d2e3
nimod7890 <nimod1234@g.skku.edu>
2024-07-29 02:03:15 +0900

    feat: 헤더 내 유저 버튼

--- packages/user/src/components/layout/top/header/index.tsx
+++ packages/user/src/components/layout/top/header/index.tsx
@@ -1,11 +1,13 @@
 import Logo from 'src/components/layout/top/header/Logo';
 import NavigateTabs from 'src/components/layout/top/header/tab';
+import UserButton from 'src/components/layout/top/header/user';
 
 export default function Header() {
 	return (
 		<header className="bg-background container flex h-[65px] items-center justify-between gap-12">
 			<Logo />
 			<NavigateTabs />
+			<UserButton />
 		</header>
 	);
 }
---
+++ packages/user/src/components/layout/top/header/user/index.tsx
@@ -0,0 +1,24 @@
+import UserIcon from 'src/assets/icons/user.svg?react';
+import SpeechBubble from 'src/components/layout/top/header/user/SpeechBubble';
+import useAuth from 'src/hooks/useAuth';
+
+export default function UserButton() {
+	const { user } = useAuth();
+
+	// TODO: 로그인, 로그아웃
+
+	return (
+		<div className="flex items-center gap-2">
+			{user ? (
+				<p>{user.name}님 반갑습니다</p>
+			) : (
+				<>
+					<SpeechBubble />
+					<button type="button" aria-label="user-icon" className="p-[10px]">
+						<UserIcon />
+					</button>
+				</>
+			)}
+		</div>
+	);
+}

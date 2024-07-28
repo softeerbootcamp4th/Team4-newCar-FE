5c08aecfbf286bb9a231bb570ed2f4cf1be7f304
nimod7890 <nimod1234@g.skku.edu>
2024-07-28 22:18:10 +0900

    design: container에 색상 제거

--- packages/user/src/components/layout/top/TopSectionContainer.tsx
+++ packages/user/src/components/layout/top/TopSectionContainer.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export default function TopSectionContainer({ children }:PropsWithChildren) {
 	return (
-		<div className="bg-white sticky top-0">
+		<div className="sticky top-0">
 			{children}
 		</div>
 	);

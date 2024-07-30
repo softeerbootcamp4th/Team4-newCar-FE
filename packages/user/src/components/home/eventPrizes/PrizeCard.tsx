8a6510d934c225df381cf136aed0677eb9765b2e
nimod7890 <nimod1234@g.skku.edu>
2024-07-30 10:50:01 +0900

    feat: 이벤트 경품 설명 이미지에 alt tag 추가
    (cherry picked from commit e54ca6334596d3a80a2f50ee0d5ba00a5369e501)

--- packages/user/src/components/home/eventPrizes/PrizeCard.tsx
+++ packages/user/src/components/home/eventPrizes/PrizeCard.tsx
@@ -13,7 +13,11 @@ export default function PrizeCard({
 	return (
 		<div className="relative flex flex-col items-center justify-center gap-4 rounded-[10px] bg-neutral-700 px-4 pb-[21px] pt-9">
 			<div className="bg-primary absolute -top-5 rounded-[17px] px-3 py-[5px]">{drawCount}명</div>
-			<img className="h-[90px] w-[180px] object-contain" src={imageUrl} />
+			<img
+				alt={`${drawCount}명 상품`}
+				className="h-[90px] w-[180px] object-contain"
+				src={imageUrl}
+			/>
 			<p className="text-body-3 w-[147px] text-center font-medium">{children}</p>
 		</div>
 	);

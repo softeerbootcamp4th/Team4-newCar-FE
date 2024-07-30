ea87fb2cd886fd07e1a8a9750becfa0de9886566
nimod7890 <nimod1234@g.skku.edu>
2024-07-30 11:27:36 +0900

    feat: 선착순 퀴즈 섹션 description box 구현
    (cherry picked from commit a72afe8afcb4bf9a4cae5f89bd89473ddc403517)

---
+++ packages/user/src/components/home/fastestQuiz/Description.tsx
@@ -0,0 +1,12 @@
+export default function Description() {
+	return (
+		<div className="bg-background flex flex-col items-center rounded-[10px] px-[110px] py-[30px]">
+			<p className="font-medium">
+				매일 <strong>오후 3시 15분</strong> 깜짝 스피드 퀴즈가 진행됩니다.
+			</p>
+			<p className="font-medium">
+				가장 먼저 정답을 맞히고 <strong>스타벅스 1만 원권</strong>을 받아가세요!
+			</p>
+		</div>
+	);
+}

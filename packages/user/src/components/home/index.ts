9589e766ce6bab344c47e32b9e34663d4445b484
nimod7890 <nimod1234@g.skku.edu>
2024-07-30 11:42:24 +0900

    feat: 선착순 퀴즈 섹션 구현
    (cherry picked from commit 26249831289b9a9f84a2b4cacd52111bbf8cc4c6)

--- packages/user/src/components/home/index.ts
+++ packages/user/src/components/home/index.ts
@@ -1,2 +1,3 @@
 export { default as EventGuidelines } from './EventGuidelines';
-export { default as EventPrizes } from './eventPrizes';
+export { default as EventPrizes } from './EventPrizes';
+export { default as FastestQuiz } from './fastestQuiz';

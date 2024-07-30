6bfa8c5e0bd28b1229841ea38a63f475607d0305
nimod7890 <nimod1234@g.skku.edu>
2024-07-31 00:05:03 +0900

    design: scroll snap

--- packages/user/src/components/home/fastestQuiz/index.tsx
+++ packages/user/src/components/home/fastestQuiz/index.tsx
@@ -6,7 +6,7 @@ import TopSection from './TopSection';
 /** 선착순 퀴즈 설명 섹션 */
 export default function FastestQuiz() {
 	return (
-		<section id={QUIZ_SECTION_ID} className="bg-neutral-700 py-[120px]">
+		<section id={QUIZ_SECTION_ID} className="bg-neutral-700 pt-[120px]">
 			<div className="gap-15 flex flex-col items-center">
 				<TopSection />
 				<QuizImages />

96c8eb3af6ed6fec20a7da5ce29832d5124bd08e
nimod7890 <nimod1234@g.skku.edu>
2024-07-30 18:01:31 +0900

    feat: 선착순 퀴즈 힌트 섹션 구현

--- packages/user/src/components/home/index.ts
+++ packages/user/src/components/home/index.ts
@@ -1,3 +1,4 @@
 export { default as EventGuidelines } from './EventGuidelines';
 export { default as EventPrizes } from './EventPrizes';
 export { default as FastestQuiz } from './fastestQuiz';
+export { default as QuizHint } from './quizHint';

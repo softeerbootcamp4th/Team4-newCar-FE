223e0b3b7e54838b9efd44f69902b12b84ced335
nimod7890 <nimod1234@g.skku.edu>
2024-07-30 23:22:38 +0900

    feat: 이벤트 단계 사이에 화살표 divider 자동 삽입하는 stack container 생성

---
+++ packages/user/src/components/home/eventSteps/ContentsContainer.tsx
@@ -0,0 +1,15 @@
+import { Children, Fragment, PropsWithChildren } from 'react';
+import StepArrow from 'src/assets/icons/step-arrow.svg?react';
+
+export default function ContentsContainer({ children }: PropsWithChildren) {
+	const contents = Children.toArray(children);
+
+	return (
+		<div className="flex items-center gap-5 p-10 border border-primary">
+			{contents.map((content, index) => (<Fragment key={index}>
+					{content}
+					{index < contents.length - 1 && <StepArrow />}
+				</Fragment>))}
+		</div>
+	);
+}

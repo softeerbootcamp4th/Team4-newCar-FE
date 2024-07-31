812224c7a10dd53d6f2b9fa817eb5e8e78e1bc46
admin <admin@adminui-MacBookPro.local>
2024-07-31 16:39:00 +0900

    feat(Hamburger): 좌측 상단 햄버거 버튼 위치 고정 및 중앙 컨턴츠 사이즈,위치 수정

--- packages/admin/src/layouts/appLayout.tsx
+++ packages/admin/src/layouts/appLayout.tsx
@@ -5,12 +5,14 @@ import SystemContainer from 'src/components/appLayout/System';
 
 export default function AppLayout() {
 	return (
-		<div className="mx-auto mt-5 flex h-screen w-full max-w-[1200px] gap-10">
+		<div className="mx-auto mt-5 flex h-screen w-screen min-w-[1200px] gap-10">
 			<SideBarContainer />
-			<SystemContainer />
-			<div className="mt-8 flex w-full flex-col">
-				<Header headerTitle="이벤트 등록" />
-				<Outlet />
+			<div className="mx-auto mt-5 flex h-screen w-full max-w-[1200px] gap-10">
+				<SystemContainer />
+				<div className="mb-8 mt-8 flex w-full flex-col border-4 border-blue-50 p-4">
+					<Header headerTitle="이벤트 등록" />
+					<Outlet />
+				</div>
 			</div>
 		</div>
 	);

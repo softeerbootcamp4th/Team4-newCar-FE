ed83aca0b40a6781d3ab5d13b7135f9dcb865af9
nimod7890 <nimod1234@g.skku.edu>
2024-07-31 23:00:50 +0900

    feat: team card 공통 부분 구현

--- packages/user/src/styles/theme.ts
+++ packages/user/src/styles/theme.ts
@@ -137,6 +137,7 @@ const fontWeight = {
 };
 
 const backgroundImage = {
+	'gradient-to-b-card': 'linear-gradient(to bottom, var(--tw-gradient-stops) 50%)',
 	'gradient-lineBanner': 'linear-gradient(to right, #2589FF, #232AA3)',
 	'gradient-cards1': 'linear-gradient(to right, #FD7A4D, #000000)',
 	'gradient-cards3': 'linear-gradient(to right, #37392A, #000000)',

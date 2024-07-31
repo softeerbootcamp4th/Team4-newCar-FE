37db0c328c6620b85965e4cc8245c39d33cba0c2
admin <admin@adminui-MacBookPro.local>
2024-07-31 16:04:57 +0900

    feat(Header): 라우팅 페이지 header 동적으로 변경 구현

--- packages/admin/src/layouts/appLayout.tsx
+++ packages/admin/src/layouts/appLayout.tsx
@@ -1,15 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useMatches } from 'react-router-dom';
 import Header from 'src/components/appLayout/Header';
 import SideBarContainer from 'src/components/appLayout/SideBarContainer';
 import SystemContainer from 'src/components/appLayout/System';
 
 export default function AppLayout() {
+	const matches = useMatches();
+	const location = useLocation();
+	const matchResult = matches
+		.filter((match) => match.id !== '0')
+		.find((match) => match.pathname === location.pathname);
+	const headerTitle = matchResult?.id ?? '';
+
 	return (
 		<div className="mx-auto mt-5 flex h-screen w-full max-w-[1200px] gap-10">
 			<SideBarContainer />
 			<SystemContainer />
 			<div className="mt-8 flex w-full flex-col">
-				<Header headerTitle="이벤트 등록" />
+				<Header headerTitle={headerTitle} />
 				<Outlet />
 			</div>
 		</div>

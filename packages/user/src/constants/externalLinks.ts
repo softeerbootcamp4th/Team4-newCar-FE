c65d18ea76869a5a96bd3eb007f71ece83f09a64
nimod7890 <nimod1234@g.skku.edu>
2024-08-01 11:54:16 +0900

    refactor: 외부 링크 constants 로 관리

---
+++ packages/user/src/constants/externalLinks.ts
@@ -0,0 +1,10 @@
+const EXTERNAL_LINKS = {
+	CASPER_HOMEPAGE: 'https://casper.hyundai.com/vehicles/electric/highlight',
+	HYUNDAI_HOMEPAGE: 'https://www.hyundai.com',
+	HYUNDAI_CERTIFICATE: 'https://casper.hyundai.com/certificate',
+	HYUNDAI_COPYRIGHT: 'https://www.hyundai.com/kr/ko/copyright',
+	HYUNDAI_POLICY: 'https://privacy.hyundai.com/overview/full-policy',
+	CASPER_AGREEMENTS: 'https://casper.hyundai.com/agreements',
+} as const;
+
+export default EXTERNAL_LINKS;

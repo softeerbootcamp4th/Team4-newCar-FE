0457923a6e472a4c5183eb4d7ce26857c24c8d55
admin <admin@adminui-MacBookPro.local>
2024-08-01 11:21:11 +0900

    fix(Winner): 우승자 페이지 리팩토링 및 테이블 레이아웃 수정

--- packages/admin/src/pages/winner/WinnerResult.tsx
+++ packages/admin/src/pages/winner/WinnerResult.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Pagination from 'src/components/common/Pagination';
 import Tab from 'src/components/common/Tab';
 import ResultTable from 'src/components/table/ResultTable';
@@ -11,45 +11,63 @@ const TabName = {
 };
 
 const racingHeaders = [
-	{ text: '당첨 등수', width: 100 },
-	{ text: '유저 이름', width: 100 },
-	{ text: '유저 전화번호', width: 100 },
-	{ text: '공유 링크 접속 ', width: 100 },
-	{ text: '유형카드 ', width: 100 },
+	{ text: '당첨 등수', width: '20%' },
+	{ text: '유저 이름', width: '20%' },
+	{ text: '유저 전화번호', width: '20%' },
+	{ text: '공유 링크 접속 ', width: '20%' },
+	{ text: '유형카드 ', width: '20%' },
 ];
 
 const quizHeaders = [
-	{ text: '유저 이름', width: 100 },
-	{ text: '유저 전화번호', width: 100 },
-	{ text: '당첨 날짜 ', width: 100 },
+	{ text: '유저 이름', width: '33%' },
+	{ text: '유저 전화번호', width: '33%' },
+	{ text: '당첨 날짜 ', width: '33%' },
 ];
 
-const getRows = (pageIndex: number, rawList: RacingWinner[]) =>
-	rawList
+const getRows = (pageIndex: number, rawList: RacingWinner[]) => {
+	const rows = rawList
 		.slice(pageIndex * 10, pageIndex * 10 + 10)
 		.map((racingWinner) => Object.values(racingWinner));
+	while (rows.length < 10) {
+		rows.push(['ㅤ']);
+	}
+	return rows;
+};
 
 function WinnerResult() {
 	const { racingWinners } = useEvent();
 	const [pageIndex, setPageIndex] = useState(0);
 	const [tabName, setTabName] = useState(TabName.QUIZ);
+	const [total, setTotal] = useState(0);
+	const [rows, setRows] = useState<string[][]>([]);
+	const [headers, setHeaders] = useState<{ text: string; width: string }[]>([]);
 
-	const racingRows = racingWinners ? getRows(pageIndex, racingWinners) : [];
-	// quizwinner mock 준비중 나오면 racingWinner => quizWinner 교체
-	const quizRows = racingWinners ? getRows(pageIndex, racingWinners) : [];
+	useEffect(() => {
+		if (tabName === TabName.QUIZ && racingWinners) {
+			setTotal(racingWinners.length);
+			setRows(getRows(pageIndex, racingWinners));
+			setHeaders(quizHeaders);
+		}
+		if (tabName === TabName.RACE && racingWinners) {
+			setTotal(racingWinners.length);
+			setRows(getRows(pageIndex, racingWinners));
+			setHeaders(racingHeaders);
+		}
+	}, [racingWinners, pageIndex]);
 
-	const headers = tabName === TabName.QUIZ ? quizHeaders : racingHeaders;
-	const rows = tabName === TabName.QUIZ ? quizRows : racingRows;
+	useEffect(() => {
+		setPageIndex(0);
+	}, [tabName]);
 
 	return (
-		<div className="flex w-[600px] flex-col gap-2">
+		<div className="flex h-full w-full flex-col gap-4">
 			<Tab
 				tabNames={[TabName.QUIZ, TabName.RACE]}
 				setSelectedTabName={setTabName}
 				selectedTabName={tabName}
 			/>
 			<ResultTable headers={headers} rows={rows} />
-			<Pagination pageIndex={pageIndex} setPageIndex={setPageIndex} total={21} />
+			<Pagination pageIndex={pageIndex} setPageIndex={setPageIndex} total={total} />
 		</div>
 	);
 }

2ed66874e4329b59e62de76257ea045353dcc6aa
nimod7890 <nimod1234@g.skku.edu>
2024-08-04 17:49:22 +0900

    feat: 링크 복사 시 toast 띄우기

--- packages/user/src/components/common/toast/Toast.tsx
+++ packages/user/src/components/common/toast/Toast.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+// eslint-disable @typescript-eslint/comma-dangle
 import * as ToastPrimitives from '@radix-ui/react-toast';
 import { cva, type VariantProps } from 'class-variance-authority';
 import React from 'react';
@@ -25,7 +27,7 @@ const toastVariants = cva(
 	{
 		variants: {
 			variant: {
-				default: 'bg-background shadow-md',
+				default: 'bg-foreground text-background',
 			},
 		},
 		defaultVariants: {
@@ -37,15 +39,13 @@ const toastVariants = cva(
 const Toast = React.forwardRef<
 	React.ElementRef<typeof ToastPrimitives.Root>,
 	React.ComponentPropsWithoutRef<typeof ToastPrimitives.Root> & VariantProps<typeof toastVariants>
->(({ className, variant, ...props }, ref) => {
-	return (
-		<ToastPrimitives.Root
-			ref={ref}
-			className={cn(toastVariants({ variant }), className)}
-			{...props}
-		/>
-	);
-});
+>(({ className, variant, ...props }, ref) => (
+	<ToastPrimitives.Root
+		ref={ref}
+		className={cn(toastVariants({ variant }), className)}
+		{...props}
+	/>
+));
 Toast.displayName = ToastPrimitives.Root.displayName;
 
 const ToastAction = React.forwardRef<

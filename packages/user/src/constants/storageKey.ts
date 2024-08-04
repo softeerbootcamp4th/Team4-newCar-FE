f1993afe8c4f2a453f34e3c41b94c727a10f34e8
nimod7890 <nimod1234@g.skku.edu>
2024-08-04 15:50:29 +0900

    refactor: hook 외부에서 storage key 사용 가능하도록 분리

---
+++ packages/user/src/constants/storageKey.ts
@@ -0,0 +1,8 @@
+const STORAGE_KEY_PREFIX = 'hyundai-softeer-team4';
+
+const STORAGE_KEYS = {
+	USER: `${STORAGE_KEY_PREFIX}-user`,
+  TOKEN: `${STORAGE_KEY_PREFIX}-token`,
+} as const;
+
+export default STORAGE_KEYS;

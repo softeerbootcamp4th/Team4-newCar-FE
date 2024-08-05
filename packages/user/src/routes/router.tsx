0808b679501b7500b11c8087816e18ca1cebe9eb
nimod7890 <nimod1234@g.skku.edu>
2024-08-05 19:53:08 +0900

    feat: 이벤트 페이지 lazy loading

--- packages/user/src/routes/router.tsx
+++ packages/user/src/routes/router.tsx
@@ -1,8 +1,11 @@
+import { lazy, Suspense } from 'react';
 import { createBrowserRouter, RouteObject } from 'react-router-dom';
 import Layout from 'src/components/layout/index.tsx';
 import RoutePaths from 'src/constants/routePath.ts';
 import AuthProvider from 'src/context/auth/index.tsx';
 import HomePage from 'src/pages/HomePage.tsx';
+
+const EventPage = lazy(() => import('src/pages/EventPage.tsx'));
 
 const routes: RouteObject[] = [
 	{
@@ -19,7 +22,12 @@ const routes: RouteObject[] = [
 			},
 			{
 				path: RoutePaths.Event,
-				element: <>event</>,
+				element: (
+					// TODO: event page loading component
+					<Suspense fallback={<>불러오는 중...</>}>
+						<EventPage />
+					</Suspense>
+				),
 			},
 		],
 	},

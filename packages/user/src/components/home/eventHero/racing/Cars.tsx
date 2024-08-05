0979732c73d0d6fb8ebc0dc2e8c007101cd2795b
nimod7890 <nimod1234@g.skku.edu>
2024-08-05 19:47:06 +0900

    perf: 용량 500kb 넘는 svg 이미지 -> png로 변경

--- packages/user/src/components/home/eventHero/racing/Cars.tsx
+++ packages/user/src/components/home/eventHero/racing/Cars.tsx
@@ -1,10 +1,6 @@
-/* layer 순서 */
-import SecondCarImage from 'src/assets/landing-racing/khaki-car.svg?react'; // 2
-import SecondCarShadow from 'src/assets/landing-racing/khaki-shadow.svg?react'; // 5
-import FirstCarImage from 'src/assets/landing-racing/orange-car.svg?react'; // 1
-import FirstCarShadow from 'src/assets/landing-racing/orange-shadow.svg?react'; // 4
-import ThirdCarImage from 'src/assets/landing-racing/white-car.svg?react'; // 3
-import ThirdCarShadow from 'src/assets/landing-racing/white-shadow.svg?react'; // 6
+import SecondCarShadow from 'src/assets/landing-racing/khaki-shadow.svg?react';
+import FirstCarShadow from 'src/assets/landing-racing/orange-shadow.svg?react';
+import ThirdCarShadow from 'src/assets/landing-racing/white-shadow.svg?react';
 
 export default function Cars() {
 	return (
@@ -19,7 +15,11 @@ function FirstCar() {
 	return (
 		<div className="absolute left-[389px] h-full w-[490px]">
 			<FirstCarShadow className="absolute bottom-0" />
-			<FirstCarImage className="animate-rotate1 absolute bottom-[40px] left-[4px] z-30" />
+			<img
+				alt="1등을 달리고 있는 차"
+				src="src/assets/landing-racing/orange-car.png"
+				className="animate-rotate1 absolute bottom-[40px] left-[4px] z-30"
+			/>
 		</div>
 	);
 }
@@ -28,7 +28,11 @@ function SecondCar() {
 	return (
 		<div className="absolute left-[170px] h-full w-[370px]">
 			<SecondCarShadow className="absolute bottom-[25px] left-[25px]" />
-			<SecondCarImage className="animate-rotate2 absolute bottom-[70px] z-20" />
+			<img
+				alt="2등을 달리고 있는 차"
+				src="src/assets/landing-racing/khaki-car.png"
+				className="animate-rotate2 absolute bottom-[70px] z-20"
+			/>
 		</div>
 	);
 }
@@ -37,7 +41,11 @@ function ThirdCar() {
 	return (
 		<div className="absolute right-[190px] h-full w-[210px]">
 			<ThirdCarShadow className="absolute bottom-[54px] right-[20px]" />
-			<ThirdCarImage className="animate-rotate3 absolute bottom-[70px] right-0 z-10" />
+			<img
+				alt="3등을 달리고 있는 차"
+				src="src/assets/landing-racing/white-car.png"
+				className="animate-rotate3 absolute bottom-[70px] right-0 z-10"
+			/>
 		</div>
 	);
 }

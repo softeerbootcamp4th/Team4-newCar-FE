f86a958241c8254b6124f155b722aef269a238f3
nimod7890 <nimod1234@g.skku.edu>
2024-08-06 19:26:28 +0900

    design: 비속어 작성 경고 문구 font size 변경

--- packages/user/src/components/event/chatting/inputArea/index.tsx
+++ packages/user/src/components/event/chatting/inputArea/index.tsx
@@ -4,7 +4,7 @@ export default function ChatInputArea() {
 	return (
 		<div className="mb-[54px] flex flex-col items-center gap-[5px]">
 			{/* Todo: 비속어 작성 횟수 불러오기 */}
-			<p className="text-[#FF3C76]">
+			<p className="text-detail-2 text-[#FF3C76]">
 				비속어 혹은 부적절한 기대평을 5회 이상 작성할 경우, 댓글 작성이 제한됩니다.
 			</p>
 			<ChatInput />

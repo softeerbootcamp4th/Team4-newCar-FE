199891c33811a201b113d3e8c71a9b1acec049d9
nimod7890 <nimod1234@g.skku.edu>
2024-08-08 22:22:22 +0900

    feat: 캐스퍼 레이싱 페이지 랜딩 뷰 레이싱 섹션으로 고정

---
+++ packages/user/src/utils/scrollToElementId.tsx
@@ -0,0 +1,7 @@
+export default function scrollToElementId(sectionId: string) {
+		const element = document.getElementById(sectionId);
+		if (element) {
+			element.scrollIntoView({ behavior: 'smooth', inline: 'center' });
+			window.history.replaceState({}, '', null);
+		}
+}

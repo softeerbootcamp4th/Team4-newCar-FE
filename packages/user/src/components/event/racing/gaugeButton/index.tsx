3c812c5a402c545338fb89d09703af56b1a936b0
nimod7890 <nimod1234@g.skku.edu>
2024-08-09 22:32:19 +0900

    feat: 최대 클릭 가능 수 도달할 경우 toast 띄우기

--- packages/user/src/components/event/racing/gaugeButton/index.tsx
+++ packages/user/src/components/event/racing/gaugeButton/index.tsx
@@ -1,6 +1,7 @@
 import { Category } from '@softeer/common/types';
 import { useEffect, useRef, useState } from 'react';
 import Lightning from 'src/assets/icons/lighting.svg?react';
+import { useToast } from 'src/hooks/useToast.ts';
 import Gauge from './Gauge.tsx';
 import GaugeButton from './GaugeButton.tsx';
 
@@ -13,9 +14,9 @@ interface TeamGaugeButtonProps {
 	percentage: number;
 }
 
-const MAX_CLICK = 30;
+const MAX_CLICK = 10;
 const MIN_PERCENT = 2;
-const RESET_SECOND = 1000;
+const RESET_SECOND = 10000;
 
 export default function TeamGaugeButton({
 	type,
@@ -42,6 +43,8 @@ export default function TeamGaugeButton({
 }
 
 function useGaugeProgress(originPercentage: number) {
+	const { toast } = useToast();
+
 	const [progress, setProgress] = useState(0);
 	const [clickCount, setClickCount] = useState(0);
 	const initPercentageRef = useRef(originPercentage);
@@ -61,6 +64,7 @@ function useGaugeProgress(originPercentage: number) {
 		}
 
 		if (clickCount === MAX_CLICK) {
+			toast({ description: '배터리가 떨어질 때까지 기다려주세요!' });
 			const resetTimer = setTimeout(resetToInitProgress, RESET_SECOND);
 			return () => clearTimeout(resetTimer);
 		}

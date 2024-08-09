d8b3e65649cbe9021674d10cff7474ed1ca8e6cf
nimod7890 <nimod1234@g.skku.edu>
2024-08-09 20:55:05 +0900

    perf: scrollIntoView behavior 개별 지정

--- packages/user/src/pages/EventPage.tsx
+++ packages/user/src/pages/EventPage.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { RealTimeChatting, RealTimeRacing } from 'src/components/event/index.ts';
 import SECTION_ID from 'src/constants/sectionId.ts';
-import scrollToElementId from 'src/utils/scrollToElementId.tsx';
+import scrollToElementId from 'src/utils/scrollToElementId.ts';
 
 export default function EventPage() {
 	useEffect(() => {
-		scrollToElementId(SECTION_ID.RACING);
+		scrollToElementId({ sectionId: SECTION_ID.RACING, behavior: 'instant' });
 	}, []);
 
 	return (
--- packages/user/src/utils/scrollToElementId.tsx
+++
@@ -1,7 +0,0 @@
-export default function scrollToElementId(sectionId: string) {
-	const element = document.getElementById(sectionId);
-	if (element) {
-		element.scrollIntoView({ behavior: 'instant', inline: 'center' });
-		window.history.replaceState({}, '', null);
-	}
-}

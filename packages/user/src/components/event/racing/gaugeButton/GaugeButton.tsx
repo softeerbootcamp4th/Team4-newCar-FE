a14dabfeeb4151b5af29e280a72b53907982d1de
nimod7890 <nimod1234@g.skku.edu>
2024-08-10 13:22:08 +0900

    refactor: gauge button styles

--- packages/user/src/components/event/racing/gaugeButton/GaugeButton.tsx
+++ packages/user/src/components/event/racing/gaugeButton/GaugeButton.tsx
@@ -4,54 +4,66 @@ import GradientBorderWrapper from 'src/components/common/GradientBorderWrapper.t
 import { TEAM_DESCRIPTIONS } from 'src/constants/teamDescriptions.ts';
 import type { Rank } from './index.tsx';
 
-interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
-	rank: Rank;
-	type: Category;
-	percent: number;
-}
-
-export default function GaugeButton({ type, rank, percent, disabled, ...props }: ButtonProps) {
-	const { shortTitle, title } = TEAM_DESCRIPTIONS[type];
-	const { bgStyles, fontStyles } = styles[type];
-	const imageUrl = imageUrls[type];
-
-	const displayTitle = shortTitle ?? title;
-	const formattedPercent = percent.toFixed(2);
-
-	return (
-		<button disabled={disabled} {...props} type="button" className="relative overflow-visible group disabled:opacity-70">
-			<GradientBorderWrapper className={`${!disabled && 'group-active:animate-rotate'}`}>
-				<div
-					className={`flex h-[84px] w-[250px] gap-7 rounded-[inherit] px-[14px] py-[10px] ${bgStyles}`}
-				>
-					<h2 className="pt-2">{rank}</h2>
-					<div className="flex flex-col items-center">
-						<p className={`text-body-3 font-medium ${fontStyles}`}>{formattedPercent}%</p>
-						<h6>{displayTitle}</h6>
-					</div>
-				</div>
-			</GradientBorderWrapper>
-			<img
-				src={imageUrl}
-				alt={`${title} 팀 캐스퍼 실물`}
-				className={`absolute -bottom-[25px] -right-[18px] z-10 w-[100px] cursor-pointer object-contain ${!disabled && clickTransition}`}
-			/>
-		</button>
-	);
-}
-
-const clickTransition = 'transition-transform duration-300 ease-out group-active:scale-125';
-
-const styles: Record<Category, { bgStyles: string; fontStyles: string }> = {
-	travel: { bgStyles: 'bg-gradient-cards1', fontStyles: 'text-orange-500' },
-	leisure: { bgStyles: 'bg-gradient-cards3', fontStyles: 'text-yellow-500' },
-	place: { bgStyles: 'bg-gradient-cards2', fontStyles: 'text-neutral-200' },
-	pet: { bgStyles: 'bg-gradient-cards4', fontStyles: 'text-yellow-500' },
-};
-
 const imageUrls: Record<Category, string> = {
 	travel: '/images/racing/side/travel.png',
 	leisure: '/images/racing/side/leisure.png',
 	place: '/images/racing/side/place.png',
 	pet: '/images/racing/side/pet.png',
+};
+
+interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
+	type: Category;
+	rank: Rank;
+	percent: number;
+}
+
+export default function GaugeButton({
+	type,
+	rank,
+	percent,
+	disabled = false,
+	...props
+}: ButtonProps) {
+	const { shortTitle, title } = TEAM_DESCRIPTIONS[type];
+	const imageUrl = imageUrls[type];
+
+	const displayTitle = shortTitle ?? title;
+	const formattedPercent = percent.toFixed(2);
+
+	const styles = getStyles({ type, isActive: !disabled });
+
+	return (
+		<button type="button" disabled={disabled} className={styles.button} {...props}>
+			<GradientBorderWrapper className={styles.borderWrapper}>
+				<div className={styles.innerborderWrapper}>
+					<h2 className="pt-2">{rank}</h2>
+					<div className="flex flex-col items-center">
+						<p className={styles.percent}>{formattedPercent}%</p>
+						<h6>{displayTitle}</h6>
+					</div>
+				</div>
+			</GradientBorderWrapper>
+			<img src={imageUrl} alt={`${title} 팀 캐스퍼 실물`} className={styles.image} />
+		</button>
+	);
+}
+
+function getStyles({ type, isActive }: { type: Category; isActive: boolean }) {
+	const { bgStyles, fontStyles } = styles[type];
+	const imageBaseStyles = 'absolute -bottom-[25px] -right-[18px] z-10 w-[100px] object-contain';
+
+	return {
+		percent: `text-body-3 font-medium ${fontStyles}`,
+		image: `${imageBaseStyles} ${isActive ? 'transition-transform duration-300 ease-out group-active:scale-125' : ''}`,
+		button: 'relative overflow-visible group disabled:opacity-70',
+		borderWrapper: isActive ? 'group-active:animate-rotate' : '',
+		innerborderWrapper: `flex h-[84px] w-[250px] gap-7 rounded-[inherit] px-[14px] py-[10px] ${bgStyles}`,
+	};
+}
+
+const styles: Record<Category, { bgStyles: string; fontStyles: string }> = {
+	travel: { bgStyles: 'bg-gradient-cards1', fontStyles: 'text-orange-500' },
+	leisure: { bgStyles: 'bg-gradient-cards2', fontStyles: 'text-yellow-500' },
+	place: { bgStyles: 'bg-gradient-cards3', fontStyles: 'text-neutral-200' },
+	pet: { bgStyles: 'bg-gradient-cards4', fontStyles: 'text-yellow-500' },
 };

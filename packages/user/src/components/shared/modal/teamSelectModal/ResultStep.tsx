b55f7e27a9855784e93de5d12daac07dd9e1c340
nimod7890 <nimod1234@g.skku.edu>
2024-08-13 01:24:02 +0900

    refactor: 유형 검사 결과 타이틀 리팩토링

--- packages/user/src/components/shared/modal/teamSelectModal/ResultStep.tsx
+++ packages/user/src/components/shared/modal/teamSelectModal/ResultStep.tsx
@@ -1,4 +1,5 @@
 import { Category } from '@softeer/common/types';
+import { FunctionComponent, PropsWithChildren } from 'react';
 import LinkShare from 'src/components/shared/linkShare/index.tsx';
 import RacingTeamCard from 'src/components/shared/RacingTeamCard.tsx';
 import { TEAM_DESCRIPTIONS } from 'src/constants/teamDescriptions.ts';
@@ -7,22 +8,24 @@ import type { SubmitQuizAnswersResponse } from 'src/hooks/query/useSubmitTeamTyp
 interface ResultStepProps extends SubmitQuizAnswersResponse {}
 
 export default function ResultStep({ team: type }: ResultStepProps) {
-	const { details } = TEAM_DESCRIPTIONS[type];
-	const title = TITLE[type];
+	const { title, shortTitle, details } = TEAM_DESCRIPTIONS[type];
+	const displayTitle = shortTitle ?? title;
+
+	const CategoryTitleTemplate = TITLE[type];
 	const { titleStyles, detailsStyles } = styles[type];
 
 	return (
-		<div className="grid h-full grid-flow-row items-center justify-start gap-11 p-[30px] sm:p-[50px] md:grid-flow-col lg:p-[70px]">
+		<div className="grid h-full gap-11 p-8 sm:p-12 md:grid-flow-col lg:p-16">
 			<RacingTeamCard type={type} size="modal" />
-			<div className="flex h-full max-w-[400px] flex-col items-start justify-between gap-10 pb-[50px] pt-[20px] sm:max-w-[500px] md:max-h-[400px] md:pb-[10px] lg:max-w-[490px]">
+			<div className="flex h-full max-w-lg flex-col justify-between gap-10 pt-5 pb-12 sm:max-w-xl md:max-h-[400px] md:pb-2">
 				<div>
 					<p className={`${titleStyles} text-heading-8 mb-6 whitespace-pre-line font-bold`}>
-						{title}
+						<CategoryTitleTemplate><strong className="text-foreground">{displayTitle}</strong></CategoryTitleTemplate>
 					</p>
-					<p className={`text-body-3 ${detailsStyles} break-words break-all`}>{details}</p>
+					<p className={`text-body-3 ${detailsStyles} break-words`}>{details}</p>
 				</div>
-				<div className="w-full">
-					<p className="text-body-3 mb-4 w-full">내 링크 공유하고 추첨 당첨확률을 높여보세요!</p>
+				<div>
+					<p className="text-body-3 mb-4">내 링크 공유하고 추첨 당첨확률을 높여보세요!</p>
 					<LinkShare category={type} />
 				</div>
 			</div>
@@ -30,31 +33,31 @@ export default function ResultStep({ team: type }: ResultStepProps) {
 	);
 }
 
-const TITLE = {
-	pet: (
+const TITLE: Record<Category, FunctionComponent<PropsWithChildren>> = {
+	pet: ({ children }) => (
 		<>
 			당신은 동물을 사랑하는
 			<br />
-			<strong className="text-foreground">펫 프렌들리</strong> 유형!
+			{children} 유형!
 		</>
 	),
-	place: (
+	place: ({ children }) => (
 		<>
-			차량 내부 <strong className="text-foreground">공간 활용</strong>이<br /> 뛰어난 유형!
+			차량 내부 {children}이<br /> 뛰어난 유형!
 		</>
 	),
-	travel: (
+	travel: ({ children }) => (
 		<>
 			캠핑과 피크닉을 즐기는
 			<br />
-			<strong className="text-foreground">여행의 정석</strong> 유형!
+			{children} 유형!
 		</>
 	),
-	leisure: (
+	leisure: ({ children }) => (
 		<>
 			오프로드와 액티비티를 즐기는
 			<br />
-			<strong className="text-foreground">레저의 정석</strong> 유형!
+			{children} 유형!
 		</>
 	),
 };
@@ -66,7 +69,7 @@ const styles: Record<Category, { titleStyles: string; detailsStyles: string }> =
 	},
 	pet: {
 		titleStyles: 'text-cream-700',
-		detailsStyles: 'text-yello-100',
+		detailsStyles: 'text-yellow-100',
 	},
 	travel: {
 		titleStyles: 'text-orange-300',

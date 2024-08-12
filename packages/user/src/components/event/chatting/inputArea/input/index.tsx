ee15c6b66ee48de67cc5fe894a5a4872efea6478
nimod7890 <nimod1234@g.skku.edu>
2024-08-12 19:18:49 +0900

    feat: 로그인 안했을 경우 채팅 기능 막기

--- packages/user/src/components/event/chatting/inputArea/input/index.tsx
+++ packages/user/src/components/event/chatting/inputArea/input/index.tsx
@@ -1,17 +1,30 @@
 import { useRef } from 'react';
 import OutlinedButton from 'src/components/common/OutlinedButton.tsx';
+import LoginModal from 'src/components/shared/modal/login/index.tsx';
 import useAuth from 'src/hooks/useAuth.tsx';
+import { useToast } from 'src/hooks/useToast.ts';
 import Input from './Input.tsx';
 
+const DISABLED_CHATTING_TOAST_DESCRIPTION = '로그인 후 채팅에 참여할 수 있습니다!';
 interface ChatInputProps {
 	onSend: (message: string) => void;
 }
 export default function ChatInput({ onSend }: ChatInputProps) {
+	const { toast } = useToast();
 	const { isAuthenticated } = useAuth();
+
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
+
+		const disabledChatting = !isAuthenticated;
+
+		if (disabledChatting) {
+			toast({ description: DISABLED_CHATTING_TOAST_DESCRIPTION });
+			return;
+		}
+
 		if (inputRef.current?.value) {
 			onSend(inputRef.current.value);
 			inputRef.current.value = '';
@@ -21,10 +34,13 @@ export default function ChatInput({ onSend }: ChatInputProps) {
 	return (
 		<form className="flex items-center gap-4" onSubmit={handleSubmit}>
 			<Input ref={inputRef} name="input" required />
-			{/* Todo: 로그인해야 채팅 가능하다는 안내 tooltip 추가 */}
-			<OutlinedButton type="submit" disabled={!isAuthenticated}>
-				보내기
-			</OutlinedButton>
+			{isAuthenticated ? (
+				<OutlinedButton type="submit">
+					보내기
+				</OutlinedButton>
+			) : (
+				<LoginModal openTrigger={<OutlinedButton>로그인하고 채팅 보내기</OutlinedButton>} />
+			)}
 		</form>
 	);
 }

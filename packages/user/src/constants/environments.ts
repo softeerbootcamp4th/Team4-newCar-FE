96c28cc090a438bffda1ad98bb55c7557907ef0b
nimod7890 <nimod1234@g.skku.edu>
2024-08-12 17:06:35 +0900

    fix: vite env import 되지 않는 문제 해결

---
+++ packages/user/src/constants/environments.ts
@@ -0,0 +1,8 @@
+//* *********************************** */
+//* * Environment Variables             */
+//* *********************************** */
+
+export const {
+	VITE_BASE_URL: API_BASE_URL,
+	VITE_SOCKET_BASE_URL: SOCKET_BASE_URL,
+} = import.meta.env;

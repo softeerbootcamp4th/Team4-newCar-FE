f89d7c5241b5891fc0a086794bba1df67e0a0925
nimod7890 <nimod1234@g.skku.edu>
2024-08-12 12:02:19 +0900

    design: 반응형을 위한 dialog 기본 사이즈 조정

--- packages/user/src/components/ui/dialog.tsx
+++ packages/user/src/components/ui/dialog.tsx
@@ -29,7 +29,7 @@ const DialogOverlay = React.forwardRef<
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName;
 
 const dialogVariants = cva(
-	'rounded-6 border-2 border-khaki-500 backdrop-blur-xl data-[state=open]:animate-in data-[state=closed]:animate-out w-full h-full data-[state=closed]:fade-out-0 aspect-[16/9] data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid min-h-[200px] max-w-[960px] max-h-[540px] translate-x-[-50%] translate-y-[-50%] shadow-lg duration-200',
+	'rounded-6 border-2 w-[calc(100%-40px)] h-[calc(100%-40px)] border-khaki-500 backdrop-blur-xl data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 aspect-[16/9] data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid min-h-[200px] max-w-[960px] max-h-[540px] translate-x-[-50%] translate-y-[-50%] shadow-lg duration-200',
 	{
 		variants: {
 			variants: {
@@ -64,7 +64,7 @@ const DialogContent = React.forwardRef<
 				<DialogTitle />
 				<DialogDescription />
 			</VisuallyHidden.Root>
-			<div className="h-full overflow-scroll">{children}</div>
+			<div className="min-h-max min-w-max overflow-scroll">{children}</div>
 			<DialogPrimitive.Close className="ring-offset-background focus:ring-foreground data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-[30px] top-[30px] h-[30px] w-[30px] rounded-[2px] p-[4px] opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
 				<Close className="h-full w-full" />
 			</DialogPrimitive.Close>

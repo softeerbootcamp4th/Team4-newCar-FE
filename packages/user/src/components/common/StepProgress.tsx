cb5b1cc5c1bcfb3bff96839be9e6be0c90df7fb6
nimod7890 <nimod1234@g.skku.edu>
2024-08-12 20:10:41 +0900

    fix: unique key error
    (cherry picked from commit cb16151fe0903de6985ac97227de4765cf00fbfe)

--- packages/user/src/components/common/StepProgress.tsx
+++ packages/user/src/components/common/StepProgress.tsx
@@ -7,9 +7,9 @@ export default function StepProgress({ currentIndex, totalStep }: StepProgressPr
 		<div className="flex space-x-1 pb-10">
 			{Array(totalStep)
 				.fill(0)
-				.map((index) => (
-					// eslint-disable-next-line react/no-array-index-key
+				.map((_, index) => (
 					<div
+						// eslint-disable-next-line react/no-array-index-key
 						key={index}
 						className={`w-15 h-[3px] ${index === currentIndex ? 'bg-primary' : 'bg-neutral-300'}`}
 					/>

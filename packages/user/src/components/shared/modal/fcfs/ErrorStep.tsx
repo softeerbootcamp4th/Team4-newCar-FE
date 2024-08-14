0443e7b0b1a648749f8ea60faf7adbafb3acdf74
nimod7890 <nimod1234@g.skku.edu>
2024-08-14 13:47:11 +0900

    design: 이벤트 시작되지 않은 경우 보이는 모달 컴포넌트 문구 수정

--- packages/user/src/components/shared/modal/fcfs/ErrorStep.tsx
+++ packages/user/src/components/shared/modal/fcfs/ErrorStep.tsx
@@ -7,17 +7,17 @@ export default function ErrorStep({ error, resetErrorBoundary }: FallbackProps) 
 	const { status } = error.response;
 
 	switch (status) {
+		case 410:
+			return <ResultStep step="already-done" />;
 		case 403:
 			return (
 				<InfoStep>
-					<p className="text-heading-10 font-medium">아직 퀴즈가 시작되지 않았어요!</p>
+					<p className="text-heading-10 font-medium">이벤트가 열리지 않아 퀴즈를 볼 수 없어요!</p>
 					<Button onClick={resetErrorBoundary} variants="secondary">
-						퀴즈 다시 불러오기
+						다시 퀴즈 불러오기
 					</Button>
 				</InfoStep>
 			);
-		case 410:
-			return <ResultStep step="already-done" />;
 		default:
 			return null;
 	}

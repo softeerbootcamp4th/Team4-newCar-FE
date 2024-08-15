53d7831545b68ae5459fbf60de290864dedd0718
nimod7890 <nimod1234@g.skku.edu>
2024-08-16 01:21:00 +0900

    design: toast 간격 설정

--- packages/user/src/components/common/toast/Toast.tsx
+++ packages/user/src/components/common/toast/Toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
 	<ToastPrimitives.Viewport
 		ref={ref}
 		className={cn(
-			'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-10 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[500px]',
+			'fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-10 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[500px] gap-4',
 			className,
 		)}
 		{...props}
@@ -86,5 +86,5 @@ export {
 	ToastProvider,
 	ToastViewport,
 	type ToastActionElement,
-	type ToastProps,
+	type ToastProps
 };

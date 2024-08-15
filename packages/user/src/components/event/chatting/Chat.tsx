ff3947c37f809990356604e1409cb33d58e7cc09
nimod7890 <nimod1234@g.skku.edu>
2024-08-15 18:45:15 +0900

    fix: user id type을 변경하면서 내가 보낸 메시지임을 확인하는 과정에서 type이 맞지 않아 발생하는 오류 해결

--- packages/user/src/components/event/chatting/Chat.tsx
+++ packages/user/src/components/event/chatting/Chat.tsx
@@ -15,7 +15,7 @@ export default function Chat({ type, team, sender, content }: ChatProps) {
 			case 'm':
 			default:
 				return (
-					<Message sender={sender} team={team} isMyMessage={me?.id === sender}>
+					<Message sender={sender} team={team} isMyMessage={me?.id === sender.toString()}>
 						{content}
 					</Message>
 				);

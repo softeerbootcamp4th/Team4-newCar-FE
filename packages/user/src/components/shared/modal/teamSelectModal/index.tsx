83151005388af1c4ac601f702d92e972fe50d20c
nimod7890 <nimod1234@g.skku.edu>
2024-08-15 14:35:55 +0900

    feat: 유형 검사 모달 내 로그인 모달 열기

--- packages/user/src/components/shared/modal/teamSelectModal/index.tsx
+++ packages/user/src/components/shared/modal/teamSelectModal/index.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import Modal, { ModalProps } from 'src/components/common/Modal.tsx';
+import LoginModal from 'src/components/shared/modal/login/index.tsx';
 import PendingStep from 'src/components/shared/modal/PendingStep.tsx';
 import useAuth from 'src/hooks/useAuth.tsx';
 import TeamSelectModalContent from './ModalContent.tsx';
@@ -9,7 +10,7 @@ interface TeamSelectModalProps extends Omit<ModalProps, 'children'> {}
 export default function TeamSelectModal({ openTrigger, ...props }: TeamSelectModalProps) {
 	const { user } = useAuth();
 
-	// if (!user) return <LoginModal openTrigger={openTrigger} />;
+	if (!user) return <LoginModal openTrigger={openTrigger} />;
 
 	return (
 		<Modal variants={user?.type} openTrigger={openTrigger} {...props}>

81bd31a191b636e4e537ca54450ee2c63373d732
nimod7890 <nimod1234@g.skku.edu>
2024-08-15 18:14:26 +0900

    fix: socket subscribe 값이 trigger 될 때마다 컴포넌트가 리렌더링되는 문제 레이싱 토픽 부분 - 해결

--- packages/user/src/hooks/socket/useRacingSocket.ts
+++ packages/user/src/hooks/socket/useRacingSocket.ts
@@ -48,14 +48,18 @@ export default function useRacingSocket() {
 			setRanks(newRankStatus);
 			storeRank(newRankStatus);
 		}
-	}, [newRankStatus, ranks, storeRank]);
+	}, [newRankStatus, ranks]);
 
 	const handleStatusChange: SocketSubscribeCallbackType = useCallback((data: unknown) => {
 		const newVoteStatus = parseSocketVoteData(data as SocketData);
-		setVotes(newVoteStatus);
-	}, []);
+		const isVotesChanged = Object.keys(newVoteStatus).some(
+			(category) => newVoteStatus[category as Category] !== votes[category as Category],
+	);
 
-	const handleCarFullyCharged = (category: Category) => {
+		if (isVotesChanged) setVotes(newVoteStatus);
+	}, [votes]);
+
+	const handleCarFullyCharged = useCallback((category: Category) => {
 		const chargeData = { [categoryToSocketCategory[category]]: 1 };
 
 		const completeChargeData = Object.keys(categoryToSocketCategory).reduce(
@@ -71,7 +75,7 @@ export default function useRacingSocket() {
 			destination: RACING_SOCKET_ENDPOINTS.PUBLISH,
 			body: completeChargeData,
 		});
-	};
+	}, []);
 
 	return {
 		votes,

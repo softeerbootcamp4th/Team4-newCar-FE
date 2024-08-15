452e0648652b7601d62672d0a5498eaf69b0afaf
nimod7890 <nimod1234@g.skku.edu>
2024-08-15 17:56:16 +0900

    fix: socket subscribe 값이 trigger 될 때마다 컴포넌트가 리렌더링되는 문제 기대평 토픽 부분 - 해결(리렌더링 되면 모달도 닫힘)

--- packages/user/src/components/event/chatting/index.tsx
+++ packages/user/src/components/event/chatting/index.tsx
@@ -1,22 +1,26 @@
 import { ChatList } from '@softeer/common/components';
+import { memo } from 'react';
+import ChatInput from 'src/components/event/chatting/inputArea/input/index.tsx';
 import { UseChatSocketReturnType } from 'src/hooks/socket/useChatSocket.ts';
 import Chat from './Chat.tsx';
 import ChatInputArea from './inputArea/index.tsx';
 
 /** 실시간 기대평 섹션 */
 
-export default function RealTimeChatting({ onSendMessage, messages }: UseChatSocketReturnType) {
-	return (
-		<section className="container flex max-w-[1200px] snap-start flex-col items-center pb-[115px] pt-[50px]">
-			<h6 className="text-heading-10 mb-[25px] font-medium">기대평을 남겨보세요!</h6>
-			<ChatInputArea onSend={onSendMessage} />
-			<div className="h-[1000px] w-full overflow-y-auto rounded-[10px] bg-neutral-800 py-10">
-				<ChatList>
-					{messages.map((message) => (
-						<Chat key={message.id} {...message} />
-					))}
-				</ChatList>
-			</div>
-		</section>
-	);
-}
+const RealTimeChatting = memo(({ onSendMessage, messages }: UseChatSocketReturnType) => (
+	<section className="container flex max-w-[1200px] snap-start flex-col items-center pb-[115px] pt-[50px]">
+		<h6 className="text-heading-10 mb-[25px] font-medium">기대평을 남겨보세요!</h6>
+		<ChatInputArea>
+			<ChatInput onSend={onSendMessage} />
+		</ChatInputArea>
+		<div className="h-[1000px] w-full overflow-y-auto rounded-[10px] bg-neutral-800 py-10">
+			<ChatList>
+				{messages.map((message) => (
+					<Chat key={message.id} {...message} />
+				))}
+			</ChatList>
+		</div>
+	</section>
+));
+
+export default RealTimeChatting;

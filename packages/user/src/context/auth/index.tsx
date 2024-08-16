c22bbb2a8b56c231fb2c582ee2ff13e22f3b9538
nimod7890 <nimod1234@g.skku.edu>
2024-08-16 16:29:26 +0900

    feat: 로그아웃 시 userData 뿐만 아니라 token clear

--- packages/user/src/context/auth/index.tsx
+++ packages/user/src/context/auth/index.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren, useCallback, useMemo } from 'react';
 import AuthContext from 'src/context/auth/AuthContext.ts';
+import useTokenStorage from 'src/hooks/storage/useTokenStorage.ts';
 import useUserStorage from 'src/hooks/storage/useUserStorage.ts';
 import type { User } from 'src/types/user.d.ts';
 
 export default function AuthProvider({ children }: PropsWithChildren) {
 	const [user, setUser, clearUser] = useUserStorage();
+	const [,, clearToken] = useTokenStorage();
 
 	const setAuthData = async ({ userData }: { userData: User }) => {
 		setUser(userData);
@@ -12,6 +14,7 @@ export default function AuthProvider({ children }: PropsWithChildren) {
 
 	const clearAuthData = useCallback(() => {
 		clearUser();
+		clearToken();
 		window.location.reload();
 	}, []);
 

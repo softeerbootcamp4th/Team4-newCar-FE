637aad12465f91d7c033fa563c775d85dc684d87
nimod7890 <nimod1234@g.skku.edu>
2024-08-16 13:08:53 +0900

    feat: 로그인 시 보이는 user name에 실 데이터 표기
    (cherry picked from commit 22048deabdd67ebeed76eed5d800cd023be12df5)

--- packages/user/src/components/layout/header/user/index.tsx
+++ packages/user/src/components/layout/header/user/index.tsx
@@ -1,13 +1,16 @@
 import UserIcon from 'src/assets/icons/user.svg?react';
 import withAuth from 'src/components/shared/withAuthHOC.tsx';
+import useAuth from 'src/hooks/useAuth.tsx';
 import SpeechBubble from './SpeechBubble.tsx';
 
-const UserGreeting = withAuth(() => <p className="text-detail-1">김보민님 반갑습니다</p>);
+const UserGreeting = withAuth(({ name }:{ name:string }) => <p className="text-detail-1">{name}님 반갑습니다</p>);
 
 // TODO: 로그아웃
 export default function User() {
+	const { user } = useAuth();
 	return (
 		<UserGreeting
+			name={user?.name ?? '캐스퍼'}
 			unauthenticatedDisplay={
 				<div className="flex items-center gap-2">
 					<SpeechBubble />

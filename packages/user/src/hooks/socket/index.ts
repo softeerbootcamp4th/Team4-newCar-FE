3ff865a3f36e9ec748c9a19a3d57901d850ef711
nimod7890 <nimod1234@g.skku.edu>
2024-08-17 13:39:54 +0900

    feat: socket client 연결 중 stomp error 발생 시 throw 500 error

--- packages/user/src/hooks/socket/index.ts
+++ packages/user/src/hooks/socket/index.ts
@@ -1,35 +1,38 @@
 import { CHAT_SOCKET_ENDPOINTS, RACING_SOCKET_ENDPOINTS } from '@softeer/common/constants';
 import { useEffect } from 'react';
 import socketClient from 'src/services/socket.ts';
-import useChatSocket, { UseChatSocketReturnType } from './useChatSocket.ts';
-import useRacingSocket, { UseRacingSocketReturnType } from './useRacingSocket.ts';
+import CustomError from 'src/utils/error.ts';
+import useChatSocket from './useChatSocket.ts';
+import useRacingSocket from './useRacingSocket.ts';
 
-export type UseSocketType = {
-	chatSocket: UseChatSocketReturnType;
-	racingSocket: UseRacingSocketReturnType;
-};
+export type UseSocketReturnType = ReturnType<typeof useSocket>;
+
 export default function useSocket() {
 	const chatSocket = useChatSocket();
 
-	const { onReceiveMessage } = chatSocket;
+	const { onReceiveMessage, ...chatSocketProps } = chatSocket;
 
 	const racingSocket = useRacingSocket();
-	const { onReceiveStatus } = racingSocket;
+	const { onReceiveStatus, ...racingSocketProps } = racingSocket;
 
 	useEffect(() => {
-		socketClient.connect((isConnected) => {
-			if (isConnected) {
-				socketClient.subscribe({
-					destination: CHAT_SOCKET_ENDPOINTS.SUBSCRIBE,
-					callback: onReceiveMessage,
-				});
-				socketClient.subscribe({
-					destination: RACING_SOCKET_ENDPOINTS.SUBSCRIBE,
-					callback: onReceiveStatus,
-				});
-			}
-		});
+		if (!socketClient.isConnected) {
+			socketClient.connect((isSuccess) => {
+				if (isSuccess) {
+					socketClient.subscribe({
+						destination: CHAT_SOCKET_ENDPOINTS.SUBSCRIBE,
+						callback: onReceiveMessage,
+					});
+					socketClient.subscribe({
+						destination: RACING_SOCKET_ENDPOINTS.SUBSCRIBE,
+						callback: onReceiveStatus,
+					});
+				} else {
+					throw new CustomError('서버에서 데이터를 불러오는 데 실패했습니다.', 500);
+				}
+			});
+		}
 	}, [socketClient, onReceiveMessage, onReceiveStatus]);
 
-	return { chatSocket, racingSocket };
+	return { chatSocket: chatSocketProps, racingSocket: racingSocketProps };
 }

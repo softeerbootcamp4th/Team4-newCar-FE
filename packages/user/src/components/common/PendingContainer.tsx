0ec2f2ce2fc1b5d648ad8e7c5ef0192a0d1ae918
nimod7890 <nimod1234@g.skku.edu>
2024-08-18 02:56:15 +0900

    design: 로딩 중 보이는 캐스퍼 캐릭터 이미지 변경

--- packages/user/src/components/common/PendingContainer.tsx
+++ packages/user/src/components/common/PendingContainer.tsx
@@ -4,7 +4,7 @@ export default function PendingContainer({ message }: { message?: string }) {
 			role="status"
 			className="gap-15 flex h-screen w-screen flex-col items-center justify-center"
 		>
-			<img src="/images/fcfs/result/loading.png" alt="로딩 중 이미지" />
+			<img src="/images/fcfs/result/correct.png" alt="로딩 중 이미지" />
 			<div className="flex flex-col items-center gap-5">
 				<h4>{message || '잠시만 기다려주세요...'}</h4>
 				<p>데이터를 불러오는 중입니다. 완료되면 자동으로 이동됩니다.</p>

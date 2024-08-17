b956a6292127e4096111ceea4e0f06faf0bc8d89
nimod7890 <nimod1234@g.skku.edu>
2024-08-18 02:17:45 +0900

    feat: 유형 검사 완료했을 경우 공유 링크 생성을 위한 데이터(encryptedUserId) 받아와서 user data cookie에 저장

---
+++ packages/user/src/constants/serverMapping.ts
@@ -0,0 +1,12 @@
+import { Category } from '@softeer/common/types';
+
+export type SocketTeamEnum = 'TRAVEL' | 'SPACE' | 'LEISURE' | 'PET';
+
+const serverTeamEnumToClient:Record<SocketTeamEnum, Category> = {
+  LEISURE: 'leisure',
+  SPACE: 'place',
+  PET: 'pet',
+  TRAVEL: 'travel',
+};
+
+export default serverTeamEnumToClient;

8ae7e2f024117c882f04d7b850f51b91336b4c9a
nimod7890 <nimod1234@g.skku.edu>
2024-08-18 16:14:36 +0900

    feat: 유저 정보 불러오는 중에 error 발생한 경우 handling

--- packages/user/src/hooks/query/useGetUserInfo.ts
+++ packages/user/src/hooks/query/useGetUserInfo.ts
@@ -1,8 +1,10 @@
 import { ServerCategoryEnum } from '@softeer/common/types';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
 import useAuth from 'src/hooks/useAuth.ts';
 import http from 'src/services/api/index.ts';
 import QUERY_KEYS from 'src/services/api/queryKey.ts';
+import CustomError from 'src/utils/error.ts';
 
 export type UserResponse = {
 	userId: string;
@@ -14,11 +16,17 @@ export type UserResponse = {
 export default function useGetUserInfo() {
 	const { isAuthenticated } = useAuth();
 
-	const { data: userInfo } = useQuery<UserResponse>({
+	const { data: userInfo, status } = useQuery<UserResponse>({
 		queryKey: [QUERY_KEYS.USER_INFO],
 		queryFn: () => http.get('/user-info'),
 		enabled: isAuthenticated,
 	});
+
+	useEffect(() => {
+		if (status === 'error') {
+			throw new CustomError('유저 정보를 불러오는 중에 문제가 발생하였습니다.', 400);
+		}
+	}, [status]);
 
 	return { userInfo };
 }

535aafd384173fb53fbbc233a0e9ea8ec8a15f93
nimod7890 <nimod1234@g.skku.edu>
2024-08-18 16:05:40 +0900

    feat: 카카오 로그인 성공 시 search param에서 token 만 전달 받아 저장

--- packages/user/src/pages/KakaoRedirectPage.tsx
+++ packages/user/src/pages/KakaoRedirectPage.tsx
@@ -2,32 +2,26 @@ import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import RoutePaths from 'src/constants/routePath.ts';
 import useTokenStorage from 'src/hooks/storage/useTokenStorage.ts';
-import useUserStorage from 'src/hooks/storage/useUserStorage.ts';
 import socketManager from 'src/services/socket.ts';
 import CustomError from 'src/utils/error.ts';
 
 export default function KakaoRedirectPage() {
-	const [, setUser] = useUserStorage();
 	const [, setToken] = useTokenStorage();
 	const navigate = useNavigate();
-
 	const [searchParams] = useSearchParams();
 
 	const accessToken = searchParams.get('accessToken');
-	const userId = searchParams.get('userId');
-	const userName = searchParams.get('userName') ?? '캐스퍼';
 
 	useEffect(() => {
-		if (!accessToken || !userId) {
+		if (!accessToken) {
 			throw new CustomError('로그인 동작 중 정상적으로 유저 정보가 전달되지 않았습니다.', 400);
 		}
-		setUser({ id: userId, name: userName });
 		setToken(accessToken);
 
 		socketManager.reconnectSocketClient(accessToken);
 
 		navigate(RoutePaths.Event, { replace: true });
-	}, [accessToken, userId, userName]);
+	}, [accessToken]);
 
 	return null;
 }

244569634ad4eb6de75e8129a268ff9325a89c27
nimod7890 <nimod1234@g.skku.edu>
2024-08-19 10:36:02 +0900

    fix: build error

--- packages/user/src/pages/NotStartedEventPage.tsx
+++ packages/user/src/pages/NotStartedEventPage.tsx
@@ -14,8 +14,8 @@ export default function NotStartedEventPage() {
 		>
 			<div className="flex min-w-max flex-col items-center gap-5">
 				<h3>이벤트가 시작하기까지</h3>
-				<EventTimer endTime="h" />
-				<p className="text-detail-1 min-w-max font-medium text-neutral-300">a</p>
+				<EventTimer endTime={startTime} />
+				<p className="text-detail-1 min-w-max font-medium text-neutral-300">{formattedDuration}</p>
 			</div>
 			<img src="/images/fcfs/modal.png" alt="오류 발생 이미지" className="w-full max-w-[1000px]" />
 		</div>

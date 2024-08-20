f79d8f9ee2a2f8e1404bbedc39e08912d0386625
nimod7890 <nimod1234@g.skku.edu>
2024-08-21 00:17:47 +0900

    perf: 이벤트 경품 텍스트 p tag로 교체

--- packages/user/src/components/home/EventPrizes.tsx
+++ packages/user/src/components/home/EventPrizes.tsx
@@ -33,7 +33,7 @@ export default function EventPrizes() {
 	return (
 		<section className="gap-15 flex snap-start flex-col items-center pt-[80px]">
 			<h2>이벤트 경품</h2>
-			<h6 className="text-heading-10">캐스퍼 레이싱</h6>
+			<p className="text-heading-10 font-bold">캐스퍼 레이싱</p>
 			<div className="mb-8 flex gap-3">
 				{EVENT_PRIZES.map(({ drawCount, description }, index) => (
 					<PrizeCard

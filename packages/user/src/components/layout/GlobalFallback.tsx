a1fd1c77e99eafed6853771ae2f966237901c08c
nimod7890 <nimod1234@g.skku.edu>
2024-08-20 11:58:03 +0900

    rename: global fallback component

---
+++ packages/user/src/components/layout/GlobalFallback.tsx
@@ -0,0 +1,10 @@
+import DeferredWrapper from 'src/components/common/DeferredWrapper.tsx';
+import PendingContainer from 'src/components/common/PendingContainer.tsx';
+
+export default function GlobalFallback() {
+	return (
+		<DeferredWrapper>
+			<PendingContainer message="이벤트 정보를 불러오는 중입니다!" />;
+		</DeferredWrapper>
+	);
+}

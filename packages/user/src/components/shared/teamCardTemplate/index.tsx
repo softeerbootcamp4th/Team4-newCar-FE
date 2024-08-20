5571a609c7eafce749a3fce3add7966a55cb57d6
nimod7890 <nimod1234@g.skku.edu>
2024-08-21 00:44:34 +0900

    perf: 불필요한 h tag 제거

--- packages/user/src/components/shared/teamCardTemplate/index.tsx
+++ packages/user/src/components/shared/teamCardTemplate/index.tsx
@@ -28,7 +28,7 @@ export default function TeamCardTemplate({
 			<CardGradient variant={type} />
 			<CardContent>
 				<div className="text-center">
-					<h5 className={titleStyles}>{title}</h5>
+					<p className={titleStyles}>{title}</p>
 					<p className={cn(descriptionStyles, 'whitespace-pre-line')}>{subTitle}</p>
 				</div>
 				{children}
@@ -47,17 +47,17 @@ const styles: Record<
 > = {
 	description: {
 		cardStyles: 'h-[364px] w-[244px] pb-[21px] pt-[25px]',
-		titleStyles: '',
+		titleStyles: 'text-heading-5 font-extrabold',
 		descriptionStyles: 'text-detail-2',
 	},
 	racing: {
 		cardStyles: 'w-[160px] h-[234px] pt-[15px] pb-[18px]',
-		titleStyles: 'text-[22px] leading-[22px]',
+		titleStyles: 'text-body-2 font-extrabold leading-[22px]',
 		descriptionStyles: 'text-detail-3 leading-[15px]',
 	},
 	modal: {
 		cardStyles: 'w-[279px] h-[400px] pb-[30px] pt-[25px]',
-		titleStyles: '',
+		titleStyles: 'text-heading-5 font-extrabold',
 		descriptionStyles: 'text-detail-1',
 	},
 };

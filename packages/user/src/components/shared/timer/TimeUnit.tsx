d08af060b7a09e61539f7fb47590ef03dae1a6dc
nimod7890 <nimod1234@g.skku.edu>
2024-08-21 00:11:45 +0900

    perf: time unit tag span으로 변경

--- packages/user/src/components/shared/timer/TimeUnit.tsx
+++ packages/user/src/components/shared/timer/TimeUnit.tsx
@@ -7,10 +7,10 @@ interface TimeUnitProps {
 
 const TimeUnit = memo(({ value, label }: TimeUnitProps) => (
 	<div className="relative flex flex-col items-center gap-4">
-		<div>
-			<h4 className="bg-foreground/10 text-foreground/60 rounded-[5px] p-4 px-3 pt-6 leading-[38px] backdrop-blur-sm">
+		<div className="bg-foreground/10 rounded-[5px] p-4 px-3 pt-6 leading-[38px] backdrop-blur-sm">
+			<span className="text-heading-4 text-foreground/60 text-2xl font-extrabold">
 				{value < 10 ? `0${value}` : value}
-			</h4>
+			</span>
 		</div>
 		<p className="text-detail-2 opacity-60">{label}</p>
 	</div>

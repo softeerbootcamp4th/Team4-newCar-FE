265a76d1e19ee52e11e2e036718fc7eba1a62d72
nimod7890 <nimod1234@g.skku.edu>
2024-08-21 11:44:24 +0900

    perf: webp file public으로 이동

--- packages/user/src/components/home/eventHero/racing/Caspers.tsx
+++ packages/user/src/components/home/eventHero/racing/Caspers.tsx
@@ -15,8 +15,8 @@ function OrangeCasper() {
 			<p className="text-detail-3 text-background absolute right-[18px] top-[2px] z-10 font-medium">
 				&quot;거침없이 누구보다 빠르게&quot;
 			</p>
-			<img src="src/assets/landing-racing/casper/orange-bubble.webp" alt="여행 팀 캐스퍼 말풍선" className="absolute right-0" />
-			<img src="src/assets/landing-racing/casper/orange.webp" alt="여행 팀 캐스퍼 캐릭터" className="absolute right-[100px] top-[35px]" />
+			<img src="images/hero/casper/orange-bubble.webp" alt="여행 팀 캐스퍼 말풍선" className="absolute right-0" />
+			<img src="images/hero/casper/orange.webp" alt="여행 팀 캐스퍼 캐릭터" className="absolute right-[100px] top-[35px]" />
 		</div>
 	);
 }
@@ -27,10 +27,10 @@ function CreamCasper() {
 			<p className="text-detail-3 text-background absolute left-[20px] top-[2px] font-medium">
 				&quot;캐스퍼의 주인공은 누구?&quot;
 			</p>
-			<img src="src/assets/landing-racing/casper/cream-bubble.webp" alt="펫 팀 캐스퍼 말풍선" />
-			<img src="src/assets/landing-racing/casper/cream.webp" alt="펫 팀 캐스퍼 캐릭터" />
-			<img src="src/assets/landing-racing/casper/lighting-1.webp" alt="펫 팀 캐릭터 옆 큰 번개" className="animate-sparkle absolute -top-[2px] right-[4px]" />
-			<img src="src/assets/landing-racing/casper/lighting-2.webp" alt="펫 팀 캐릭터 옆 작은 번개" className="animate-sparkle-reverse absolute right-0 top-[22px]" />
+			<img src="images/hero/casper/cream-bubble.webp" alt="펫 팀 캐스퍼 말풍선" />
+			<img src="images/hero/casper/cream.webp" alt="펫 팀 캐스퍼 캐릭터" />
+			<img src="images/hero/casper/lighting-1.webp" alt="펫 팀 캐릭터 옆 큰 번개" className="animate-sparkle absolute -top-[2px] right-[4px]" />
+			<img src="images/hero/casper/lighting-2.webp" alt="펫 팀 캐릭터 옆 작은 번개" className="animate-sparkle-reverse absolute right-0 top-[22px]" />
 		</div>
 	);
 }
@@ -38,7 +38,7 @@ function CreamCasper() {
 function WhiteCasper() {
 	return (
 		<div className="animate-casper-float-reverse absolute left-[270px] top-[55px]">
-			<img src="src/assets/landing-racing/casper/white.webp" alt="공간 팀 캐스퍼 캐릭터" />
+			<img src="images/hero/casper/white.webp" alt="공간 팀 캐스퍼 캐릭터" />
 		</div>
 	);
 }
@@ -46,7 +46,7 @@ function WhiteCasper() {
 function KhakiCasper() {
 	return (
 		<div className="animate-casper-float absolute -top-[2px] right-[295px]">
-			<img src="src/assets/landing-racing/casper/khaki.webp" alt="레저 팀 캐스퍼 캐릭터" />
+			<img src="images/hero/casper/khaki.webp" alt="레저 팀 캐스퍼 캐릭터" />
 		</div>
 	);
 }

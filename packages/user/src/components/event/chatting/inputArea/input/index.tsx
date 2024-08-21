0abd83141627692799baaa5417a038713c6b0693
nimod7890 <nimod1234@g.skku.edu>
2024-08-22 00:53:47 +0900

    feat: 50자 제한

--- packages/user/src/components/event/chatting/inputArea/input/index.tsx
+++ packages/user/src/components/event/chatting/inputArea/input/index.tsx
@@ -38,7 +38,7 @@ export default function ChatInput({ onSend }: ChatInputProps) {
 
 	return (
 		<form className="flex items-center gap-4" onSubmit={handleSubmit}>
-			<Input ref={inputRef} name="input" required />
+			<Input ref={inputRef} maxLength={50} name="input" required />
 			<ProtectedWrapper
 				unauthenticatedDisplay={<OutlinedButton as="div">로그인하고 채팅 보내기</OutlinedButton>}
 			/>

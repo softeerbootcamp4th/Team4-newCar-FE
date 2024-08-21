e28f05e0bb520024f59d5d06368e7c84ffa0d5c0
nimod7890 <nimod1234@g.skku.edu>
2024-08-21 13:13:22 +0900

    design: 랜딩 페이지 이미지 위치 및 애니메이션 조정

--- packages/user/src/components/home/eventHero/racing/Caspers.tsx
+++ packages/user/src/components/home/eventHero/racing/Caspers.tsx
@@ -11,24 +11,24 @@ export default function Caspers() {
 
 function OrangeCasper() {
 	return (
-		<div className="animate-casper-float-reverse absolute right-[92px] top-[15px] w-[150px]">
+		<div className="animate-casper-float-reverse absolute right-[92px] top-0 w-[150px]">
 			<p className="text-detail-3 text-background absolute right-[18px] top-[2px] z-10 font-medium">
 				&quot;거침없이 누구보다 빠르게&quot;
 			</p>
 			<img src="images/hero/casper/orange-bubble.webp" alt="여행 팀 캐스퍼 말풍선" className="absolute right-0" />
-			<img src="images/hero/casper/orange.webp" alt="여행 팀 캐스퍼 캐릭터" className="absolute right-[100px] top-[35px]" />
+			<img src="images/hero/casper/orange.webp" alt="여행 팀 캐스퍼 캐릭터" className="absolute right-[100px] w-[120px] top-[35px]" />
 		</div>
 	);
 }
 
 function CreamCasper() {
 	return (
-		<div className="animate-casper-float absolute left-[37px] top-[2px] w-[220px]">
+		<div className="animate-casper-float absolute left-[37px] top-0 w-[220px]">
 			<p className="text-detail-3 text-background absolute left-[20px] top-[2px] font-medium">
 				&quot;캐스퍼의 주인공은 누구?&quot;
 			</p>
 			<img src="images/hero/casper/cream-bubble.webp" alt="펫 팀 캐스퍼 말풍선" />
-			<img src="images/hero/casper/cream.webp" alt="펫 팀 캐스퍼 캐릭터" />
+			<img src="images/hero/casper/cream.webp" alt="펫 팀 캐스퍼 캐릭터" className="absolute w-[100px] left-[90px]" />
 			<img src="images/hero/casper/lighting-1.webp" alt="펫 팀 캐릭터 옆 큰 번개" className="animate-sparkle absolute -top-[2px] right-[4px]" />
 			<img src="images/hero/casper/lighting-2.webp" alt="펫 팀 캐릭터 옆 작은 번개" className="animate-sparkle-reverse absolute right-0 top-[22px]" />
 		</div>
@@ -37,7 +37,7 @@ function CreamCasper() {
 
 function WhiteCasper() {
 	return (
-		<div className="animate-casper-float-reverse absolute left-[270px] top-[55px]">
+		<div className="animate-casper-float-reverse absolute left-[270px] top-[40px]">
 			<img src="images/hero/casper/white.webp" alt="공간 팀 캐스퍼 캐릭터" />
 		</div>
 	);

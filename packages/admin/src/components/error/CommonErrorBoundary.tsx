a0332684c1f9375faee5d2489a094f1907d2d30a
racgoo <lhsung98@naver.com>
2024-08-23 17:33:32 +0900

    fix(favicon): 파비콘 변경

--- packages/admin/src/components/error/CommonErrorBoundary.tsx
+++ packages/admin/src/components/error/CommonErrorBoundary.tsx
@@ -8,37 +8,40 @@ import { Button } from '../ui/button.tsx';
 export function CommonErrorFallback({
 	error,
 	resetErrorBoundary,
-	}: {
-		error: Error,
-		resetErrorBoundary: () => void
-	}) {
-		const navigate = useNavigate();
-		useEffect(() => {
-			switch (error.message) {
-				case '관리자 로그인을 먼저 진행해주세요':
-				case '로그인이 만료 되었습니다':
-					Cookie.clearCookie(ACCESS_TOKEN_KEY);
-					navigate('/');
-					break;
-				default:
-			}
-		}, []);
+}: {
+	error: Error;
+	resetErrorBoundary: () => void;
+}) {
+	const navigate = useNavigate();
+	useEffect(() => {
+		switch (error.message) {
+			case '관리자 로그인을 먼저 진행해주세요':
+			case '로그인이 만료 되었습니다':
+				Cookie.clearCookie(ACCESS_TOKEN_KEY);
+				navigate('/');
+				break;
+			default:
+		}
+	}, []);
 
-    return (
-		<div className="min-w-screen min-h-screen flex justify-center items-center flex-col gap-4 bg-skyblue-500 ">
-			<Button onClick={resetErrorBoundary}>다시 시도하기</Button>{error.message}
+	return (
+		<div className="min-w-screen bg-skyblue-500 flex min-h-screen flex-col items-center justify-center gap-4">
+			<Button onClick={resetErrorBoundary}>다시 시도하기</Button>
+			{error.message}
 		</div>
 	);
 }
 
 function CommonErrorBoundary({ children }: PropsWithChildren) {
-    return (
-        <ErrorBoundary
+	return (
+		<ErrorBoundary
 			FallbackComponent={CommonErrorFallback}
-			onReset={() => { window.location.reload(); }}
-        >
-            {children}
-        </ErrorBoundary>
-    );
+			onReset={() => {
+				window.location.reload();
+			}}
+		>
+			{children}
+		</ErrorBoundary>
+	);
 }
 export default CommonErrorBoundary;

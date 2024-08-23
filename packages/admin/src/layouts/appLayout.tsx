2e556a43a3bdfa03a56402175ff2b2114451436f
racgoo <lhsung98@naver.com>
2024-08-23 17:39:21 +0900

    feat(Suspense): Router Suspense Wrapping

--- packages/admin/src/layouts/appLayout.tsx
+++ packages/admin/src/layouts/appLayout.tsx
@@ -1,6 +1,6 @@
 import { ACCESS_TOKEN_KEY } from '@softeer/common/constants';
 import { Cookie } from '@softeer/common/utils';
-import { useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Header from 'src/components/appLayout/Header.tsx';
 import SideBarContainer from 'src/components/appLayout/SideBarContainer.tsx';
@@ -34,16 +34,18 @@ export default function AppLayout() {
 
 	return (
 		<CommonErrorBoundary>
-			<div className="mx-auto flex min-h-screen w-screen min-w-[1200px]">
-				{accessToken && <SideBarContainer />}
-				<div className="mx-auto flex h-full min-h-screen w-full max-w-[1200px] gap-10">
-					<SystemContainer />
-					<div className="flex w-full flex-col p-4 pb-8 pt-8">
-						<Header headerTitle={headerTitle} />
-						<Outlet />
+			<Suspense fallback="..loading">
+				<div className="mx-auto flex min-h-screen w-screen min-w-[1200px]">
+					{accessToken && <SideBarContainer />}
+					<div className="mx-auto flex h-full min-h-screen w-full max-w-[1200px] gap-10">
+						<SystemContainer />
+						<div className="flex w-full flex-col p-4 pb-8 pt-8">
+							<Header headerTitle={headerTitle} />
+							<Outlet />
+						</div>
 					</div>
 				</div>
-			</div>
+			</Suspense>
 		</CommonErrorBoundary>
 	);
 }

eecc188887b8a8186198fdec916937886f9c3fa5
nimod7890 <nimod1234@g.skku.edu>
2024-08-25 17:34:30 +0900

    feat: 채팅 기능 내 검열된 메시지 표기 문구 수정

--- packages/common/src/components/chat/BlockedChat.tsx
+++ packages/common/src/components/chat/BlockedChat.tsx
@@ -1,7 +1,7 @@
 export default function BlockedChat() {
 	return (
 		<div className="rounded-7 px-15 flex items-center justify-center bg-neutral-600 py-[10px] opacity-50">
-			자동 검열 기준에 의해 게시 중단된 기대평입니다.
+			관리자에 의해 게시 중단된 기대평입니다.
 		</div>
 	);
 }
